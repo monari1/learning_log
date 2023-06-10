526b7bb4d59e44090ac46822a11bc96f9f75406b
monari1 <francisnyasinga@gmail.com>
2023-06-10 21:54:38 +0300

    added the topics to views ready to be rendered on html

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -5,6 +5,6 @@ from django.urls import path
 app_name = 'learning_logs'
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('topics', views.topics, name = 'topics')
+    path('topics', views.topics, name = 'topics'),
 ]
 
