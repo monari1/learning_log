f7dba080d6115f1a0c57b7a71f13a5e55d595c62
monari1 <francisnyasinga@gmail.com>
2023-06-10 21:50:02 +0300

    start on making topics page. First, we created the path

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 
 app_name = 'learning_logs'
 urlpatterns = [
-    path('', views.index, name = 'index')
+    path('', views.index, name = 'index'),
+    path('topics', views.topics, name = 'topics')
 ]
 
