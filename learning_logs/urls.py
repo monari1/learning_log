5447db9be724092d5459c15ae4e3f56ad3acc7b5
monari1 <francisnyasinga@gmail.com>
2023-06-22 23:53:03 +0300

    creating url for the form

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('', views.index, name = 'index'),
     path('topics', views.topics, name = 'topics'),
     path('topics/<int:topic_id>/', views.topic, name = 'topic'),
+    # Page for adding a new topic
+    path('new_topic/', views.new_topic, name='new_topic'),
 ]
 
