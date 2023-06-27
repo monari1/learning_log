85546d353e9afeeb3189eca2e1a6825d15a6447f
monari1 <francisnyasinga@gmail.com>
2023-06-27 23:38:49 +0300

    creating views for entries

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .zoom import create_zoom_meeting
 
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 # Create your views here
 def index(request):
@@ -33,6 +33,23 @@ def new_topic(request):
 
     context = {'form':form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Adding new entry"""
+    topic = Topic.objects.get(id = topic_id)
+    if request.method != 'POST':
+        form = EntryForm()
+    else: 
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topics', topic_id = topic_id)
+
+    """display if entry is blank or invalid"""
+    context = {'topic': topic,'form':form}
+    return render(request, 'learning_logs/new_entry.html', context) 
 
 def create_meeting(request):
     if request.method == 'POST':
