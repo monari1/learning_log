3030df529082c02ed145ff8e487bb24885ef1ed8
monari1 <francisnyasinga@gmail.com>
2023-07-27 21:33:06 +0300

    securing topics and entries

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -5,6 +5,8 @@ from .forms import TopicForm, EntryForm
 
 from django.contrib.auth.decorators import login_required
 
+from django.http import Http404
+
 # Create your views here
 def index(request):
     return render(request, 'learning_logs/index.html')
@@ -12,7 +14,7 @@ def index(request):
 @login_required
 def topics(request):
     # show all topics
-    topics = Topic.objects.filter(owner = request.user).order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
@@ -20,9 +22,13 @@ def topics(request):
 def topic(request, topic_id):
     """Show a single topic and all its entries"""
     topic = Topic.objects.get(id=topic_id)
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+
 
 @login_required
 def new_topic(request):
@@ -69,6 +75,8 @@ def create_meeting(request):
 def edit_entry(request, entry_id):
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         form = EntryForm(instance=entry)
